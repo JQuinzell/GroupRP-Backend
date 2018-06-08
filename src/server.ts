05bafdaae76ea671115d74a6b125335dcc668cbb
MrRainwater <jqrvz6@mst.edu>
2018-06-08 14:38:23 -0500

    basic graphql setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,52 @@
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
 import initializeDatabase from 'database'
+import Room from 'models/rooms'
+import Post from 'models/posts'
 
 const db = initializeDatabase()
+
+const typeDefs = `
+type Query {
+    rooms: [Room]
+}
+
+type Room {
+    _id: String
+    name: String
+    posts: [Post]
+}
+
+type Post {
+    _id: String
+    body: String
+    room: Room
+}
+`
+
+const resolvers = {
+    Query: {
+        rooms: () => Room.find({})
+    },
+
+    Room: {
+        posts: (room: any) => Post.find({ room: room._id})
+    }
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+const app = express()
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(3000, () => console.log('listening'))
 
 db.once('open', () => console.log('db connected'))
