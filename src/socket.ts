c52f5d88a9b39d12b76b87dbf9162b66c296e6e7
Jared Rainwater <jqrvz6@mst.edu>
2018-11-02 23:20:42 -0500

    formatting

--- src/socket.ts
+++ src/socket.ts
@@ -3,13 +3,12 @@ import * as uuid from 'uuid/v1'
 console.log('starting')
 const wss = new WebSocket.Server({ port: 5000 })
 
-
 //map socket id to its socket
 const sockets = new Map<string, WebSocket>()
 //map room ids to socket ids currently in specified room
 const rooms = new Map<string, string[]>()
 
-type Action = "JOIN_ROOM"|"SEND_MESSAGE"|"LEAVE_ROOM"
+type Action = 'JOIN_ROOM' | 'SEND_MESSAGE' | 'LEAVE_ROOM'
 
 interface Message {
     action: Action
@@ -28,43 +27,45 @@ wss.on('connection', ws => {
         console.log(data)
 
         const action = data.action
-        if(action === 'JOIN_ROOM') {
+        if (action === 'JOIN_ROOM') {
             const room = rooms.get(data.roomID)
-            if(!room) {
+            if (!room) {
                 rooms.set(data.roomID, [socketID])
             } else {
-                if(!room.find(id => id === socketID)) {
+                if (!room.find(id => id === socketID)) {
                     console.log(socketID, 'joined room', data.roomID)
                     room.push(socketID)
                 }
             }
         }
-        if(action === 'LEAVE_ROOM') {
+        if (action === 'LEAVE_ROOM') {
             const room = rooms.get(data.roomID)
-            if(room) {
+            if (room) {
                 const i = room.findIndex(id => id === socketID)
                 room.splice(i, 1)
                 console.log(room)
             }
         }
-        if(action === 'SEND_MESSAGE') {
+        if (action === 'SEND_MESSAGE') {
             const room = rooms.get(data.roomID)
             console.log(socketID, 'sending message to room', data.roomID)
-            if(room) {
+            if (room) {
                 room.forEach(id => {
                     const target = sockets.get(id)
-                    target.send(JSON.stringify({
-                        action: 'MESSAGE',
-                        roomID: data.roomID,
-                        body: data.message
-                    }))
+                    target.send(
+                        JSON.stringify({
+                            action: 'MESSAGE',
+                            roomID: data.roomID,
+                            body: data.message
+                        })
+                    )
                 })
             }
         }
     })
 
     ws.on('close', () => {
-        for(let room of rooms.values()) {
+        for (let room of rooms.values()) {
             const i = room.findIndex(id => id === socketID)
             room.splice(i, 1)
         }
@@ -73,4 +74,6 @@ wss.on('connection', ws => {
 
 wss.on('error', console.log)
 
-wss.on('listening', () => { console.log('server is listening')})
+wss.on('listening', () => {
+    console.log('server is listening')
+})
