b4ce90e6f7daddea8fc7b585916fb28a42420cee
Jared Rainwater <jqrvz6@mst.edu>
2018-11-03 19:55:41 -0500

    query all rooms

--- src/schemas/query.ts
+++ src/schemas/query.ts
@@ -1,10 +1,11 @@
-import {gql} from 'apollo-server'
+import { gql } from 'apollo-server'
 
 export default gql`
-type Query {
-    groups: [Group]
-    group(id: String): Group
-    room(id: String): Room
-    posts(room: String): [Post]
-}
+    type Query {
+        groups: [Group]
+        group(id: String): Group
+        room(id: String): Room
+        rooms(ids: [String]): [Room]
+        posts(room: String): [Post]
+    }
 `
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, gql} from 'apollo-server'
+import { ApolloServer, gql } from 'apollo-server'
 import initializeDatabase from 'database'
 import Room from 'models/rooms'
 import Post from 'models/posts'
@@ -12,27 +12,27 @@ const resolvers = {
         groups: () => Group.find({}),
         group: (_, args) => Group.findOne({ _id: args.id }),
         room: (_, args) => Room.findOne({ _id: args.id }),
+        rooms: (_, args) => Room.find({ _id: { $in: args.ids } }),
         posts: (_, args) => Post.find({ room: args.room })
     },
 
     Mutation: {
-        post: (_, {input}) => {
+        post: (_, { input }) => {
             return Post.create(input)
         }
     },
 
     Room: {
-        posts: (room) => Post.find({ room: room._id})
+        posts: room => Post.find({ room: room._id })
     },
 
     Group: {
-        rooms: (group) => Room.find({ group: group._id })
+        rooms: group => Room.find({ group: group._id })
     }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers})
+const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen()
-    .then(({url}) => console.log('listening at', url))
+server.listen().then(({ url }) => console.log('listening at', url))
 
 db.once('open', () => console.log('db connected'))
